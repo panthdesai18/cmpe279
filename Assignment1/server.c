d2365c4cdda27506455aa7c1ff0b4db4364cec94
Panth Desai <55039829+panthdesai18@users.noreply.github.com>
2020-05-06 18:07:12 -0700

    Update server.c

--- Assignment1/server.c
+++ Assignment1/server.c
@@ -5,7 +5,7 @@
 #include <stdlib.h> 
 #include <netinet/in.h> 
 #include <string.h> 
-#define PORT 8080 
+#define PORT 80 
 int main(int argc, char const *argv[]) 
 { 
     int server_fd, new_socket, valread; 
